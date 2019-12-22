bc1a8ff35beb6e7939ef90ad111b21bbd0d3e271
Jimmy <bruno_pier@hotmail.com>
2019-12-22 16:28:03 -0300

    Adding Provider & Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//Redux
+import {Provider} from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Router>
+        <Provider store={store}>
+          <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Productos} />
+                <Route exact path="/productos/nuevo" component={NuevoProducto} />
+                <Route exact path="/editar/:id" component={EditarProducto} />
+              </Switch>
+            </div>
+        </Provider>
+    </Router>
   );
 }
 
