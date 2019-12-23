e62c97347433b07af911c070929712b2357e0ced
Jimmy <bruno_pier@hotmail.com>
2019-12-23 00:30:28 -0300

    Adding values in action - rend request axios

--- src/actions/productosActions.js
+++ src/actions/productosActions.js
@@ -4,12 +4,28 @@ import {
     AGREGAR_PRODUCTO_ERROR
 } from '../types';
 
+import clienteAxios from '../config/axios';
+
 // Crear un nuevo producto - Funcion principal
 export function crearNuevoProductoAction(producto){
     return (dispatch) => {
         dispatch(nuevoProducto());
 
-        dispatch(agregarProductoExito(producto))
+        //insertar en la API
+        clienteAxios.post('/libros', producto)
+            .then(respuesta=> {
+                console.log(respuesta)
+                //si se inserta correctamente
+                dispatch(agregarProductoExito(producto));
+
+            })
+            .catch(error => {
+                console.log(error)
+
+                //si hay un error
+                dispatch(agregarProductoError(error));
+            })
+
     }
 }
 
@@ -21,3 +37,8 @@ export const agregarProductoExito = producto => ({
     type: AGREGAR_PRODUCTO_EXITO,
     payload: producto
 })
+
+export const agregarProductoError = error => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: error
+});
